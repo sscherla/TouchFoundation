d23575c30abe562a6330b2902c8dfca3f10fc9a1
Jonathan Wight <jwight@mac.com>
2011-08-10 13:42:11 -0700

    Added (YA) block based value transformer.

---
+++ Source/Extensions/CBlockValueTransformer.h
@@ -0,0 +1,15 @@
+//
+//  CBlockValueTransformer.h
+//  knotes
+//
+//  Created by Jonathan Wight on 8/10/11.
+//  Copyright (c) 2011 knotes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBlockValueTransformer : NSValueTransformer
+
+- (id)initWithBlock:(id (^)(id))inBlock reverseBlock:(id (^)(id))inReverseBlock;
+
+@end
---
+++ Source/Extensions/CBlockValueTransformer.m
@@ -0,0 +1,41 @@
+//
+//  CBlockValueTransformer.m
+//  knotes
+//
+//  Created by Jonathan Wight on 8/10/11.
+//  Copyright (c) 2011 knotes. All rights reserved.
+//
+
+#import "CBlockValueTransformer.h"
+
+@interface CBlockValueTransformer ()
+@property (readwrite, nonatomic, copy) id (^block)(id);
+@property (readwrite, nonatomic, copy) id (^reverseBlock)(id);
+@end
+
+@implementation CBlockValueTransformer
+
+@synthesize block;
+@synthesize reverseBlock;
+    
+- (id)initWithBlock:(id (^)(id))inBlock reverseBlock:(id (^)(id))inReverseBlock;
+    {
+    if ((self = [super init]) != NULL)
+        {
+        block = inBlock;
+        reverseBlock = inReverseBlock;
+        }
+    return self;
+    }
+
+- (id)transformedValue:(id)value
+    {
+    return(self.block(value));
+    }
+    
+- (id)reverseTransformedValue:(id)value
+    {
+    return(self.reverseBlock(value));
+    }
+
+@end
