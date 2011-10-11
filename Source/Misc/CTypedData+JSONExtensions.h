108d9b818d0893231b3366d7f8e0857c07801474
Jonathan Wight <jwight@mac.com>
2011-10-10 18:22:52 -0700

    Typed Data to Dictionary

---
+++ Source/Misc/CTypedData+JSONExtensions.h
@@ -0,0 +1,13 @@
+//
+//  CTypedData+JSONExtensions.h
+//  knotes
+//
+//  Created by Jonathan Wight on 10/10/11.
+//  Copyright (c) 2011 knotes. All rights reserved.
+//
+
+#import "CTypedData.h"
+
+@interface CTypedData (JSONExtensions)
+
+@end
---
+++ Source/Misc/CTypedData+JSONExtensions.m
@@ -0,0 +1,13 @@
+//
+//  CTypedData+JSONExtensions.m
+//  knotes
+//
+//  Created by Jonathan Wight on 10/10/11.
+//  Copyright (c) 2011 knotes. All rights reserved.
+//
+
+#import "CTypedData+JSONExtensions.h"
+
+@implementation CTypedData (JSONExtensions)
+
+@end
